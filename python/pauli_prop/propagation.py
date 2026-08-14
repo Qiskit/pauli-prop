6a9015c8da51ea3ec3558080e44aca59aefc48f8
Andrew Eddins <aeddins@ibm.com>
2026-08-14 12:28:19 -0700

    make `propagate_through_circuit` support Cliffords

--- python/pauli_prop/propagation.py
+++ python/pauli_prop/propagation.py
@@ -530,8 +530,17 @@ def propagate_through_circuit(
         ValueError: ``atol`` is negative.
         ValueError: ``max_terms`` is not positive.
     """
-    rot_gates = circuit_to_rotation_gates(circuit)
-    return propagate_through_rotation_gates(operator, rot_gates, max_terms, atol, frame)
+    clifford_prefix, circuit_new = evolve_through_cliffords(circuit)
+    if frame == 's':
+        paulis = operator.paulis.evolve(clifford_prefix, frame='s')
+        operator = SparsePauliOp(paulis, operator.coeffs, copy=False, ignore_pauli_phase=False)
+    rot_gates = circuit_to_rotation_gates(circuit_new)
+    operator, trunc_onenorm = propagate_through_rotation_gates(operator, rot_gates, max_terms, atol, frame)
+    if frame == 'h':
+        paulis = operator.paulis.evolve(clifford_prefix, frame='h')
+        operator = SparsePauliOp(paulis, operator.coeffs, copy=False, ignore_pauli_phase=False)
+    return operator, trunc_onenorm
+
 
 
 def propagate_through_operator(
